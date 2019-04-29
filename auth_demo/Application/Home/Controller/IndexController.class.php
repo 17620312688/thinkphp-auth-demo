5d55ad5fcd541194dbc7377e563e148d002d5105
17623012688 <386041564@qq.com>
2019-04-29 12:00:33 +0800

    first commit

---
+++ auth_demo/Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @thinkphp3.2.2
+ * @Created on 2015/08/18
+ * @Author  夏日不热    757891022@qq.com
+ *
+ */
+namespace Home\Controller;
+use Think\Controller;
+
+//权限控制类
+class IndexController extends Controller {
+	
+	//前台首页
+	public function index(){
+    	$this->display();
+	}
+	
+}
+
+
+
+
